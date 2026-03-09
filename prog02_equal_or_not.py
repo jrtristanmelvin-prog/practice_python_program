70255b30cd216c0278cbf17807c6320241ad8513
Tristanjr <jrtristanmelvin@gmail.com>
2026-03-10 00:26:33 +0800

    create the code to identify if the two numbers are equal or not.

--- prog02_equal_or_not.py
+++ prog02_equal_or_not.py
@@ -1,3 +1,9 @@
 # input num1 and num2
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
 # print if the 2 numbers are equal or not
+if num1 == num2 :
+    print("The two numbers are equal.")
+else:
+    print("The two numbers are not equal.")
