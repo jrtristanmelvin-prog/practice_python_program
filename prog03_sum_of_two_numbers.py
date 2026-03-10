fff8961a1002ca498d64cc5308565302efaef766
Tristanjr <jrtristanmelvin@gmail.com>
2026-03-10 21:00:43 +0800

    create code for the sum of 2 numbers

--- prog03_sum_of_two_numbers.py
+++ prog03_sum_of_two_numbers.py
@@ -1,3 +1,6 @@
 # input num1 and num2 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
-# add the two numbers and print the summ
+# add the two numbers and print the sum
+print(f"The sumn is {num1 + num2}")
