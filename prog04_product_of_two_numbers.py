ce582a947ecb11ccd051e78d5a7fa4ea1c11ed76
Tristanjr <jrtristanmelvin@gmail.com>
2026-03-10 21:08:34 +0800

    create the code for the product of 2 numbers

--- prog04_product_of_two_numbers.py
+++ prog04_product_of_two_numbers.py
@@ -1,3 +1,6 @@
-# input num1 and num2 
+# input num1 and num2
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
 # multuply the two numbers and print the product
+print(f"The product is {num1 * num2}")
