76e55bd0991c60c97619111dd8a62dbcf3bd165d
Tristanjr <jrtristanmelvin@gmail.com>
2026-03-10 23:56:03 +0800

    crate the code the quotient and remainder of two numbers

--- prog05_quotient_with_remainder_of_two_numbers.py
+++ prog05_quotient_with_remainder_of_two_numbers.py
@@ -1,5 +1,9 @@
 # input num1 and num2 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second numebr: "))
 
 # get the quotient of the 2 numbers and print result
+print(f"The quotient is {num1 // num2}")
 
 # get the remainder and print the result
+print(f"The remainder is {num1 % num2}")
