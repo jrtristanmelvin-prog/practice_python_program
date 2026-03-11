69ce645a3b363a1270d0684e24be9ab1fb7ed073
Tristanjr <jrtristanmelvin@gmail.com>
2026-03-11 23:56:51 +0800

    create code for the result when the first number is raised by the second number

--- prog06_first_number_is_raised_to_the_second_number.py
+++ prog06_first_number_is_raised_to_the_second_number.py
@@ -1,3 +1,6 @@
 # input num1 and num2 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
 # raise the num1 with num2 and print the result
+print(f"The result is {num1 ** num2}")
