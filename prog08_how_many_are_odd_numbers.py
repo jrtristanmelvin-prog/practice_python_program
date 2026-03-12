8a13038b63e8288020e0848bdbdeb3858d8907bb
Tristanjr <jrtristanmelvin@gmail.com>
2026-03-12 20:55:01 +0800

    create code to count how many odd numbers are there

--- prog08_how_many_are_odd_numbers.py
+++ prog08_how_many_are_odd_numbers.py
@@ -1,7 +1,14 @@
 # input 10 numbers
+counter = 0
+
+for i in range(10):
+    num = float(input("Enter a number: "))
 
 # check if odd
+    if num % 2 != 0:
 
 # count
+        counter += 1
 
 # print the counter
+print(counter)  
