26246bacd311bc783f2f713e7f10a4a31ec58b69
Tristanjr <jrtristanmelvin@gmail.com>
2026-03-13 23:19:02 +0800

    rename prog012

--- prog12_not_equal.py
+++ prog12_not_equal.py
@@ -1,3 +1,9 @@
 # input num1 and num2 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
 # compare the 2 numbers and print equal when numbers are the same
+if num1 == num2:
+    print("The numbers are equal.")
+else:
+    print("The numbers are not equal.")
