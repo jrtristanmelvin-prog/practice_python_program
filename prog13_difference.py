5f08039c4702b5b3d90bf0c95a2fa3975ae4cf88
Tristanjr <jrtristanmelvin@gmail.com>
2026-03-20 21:27:55 +0800

    create program that print difference of 2 numbers

---
+++ prog13_difference.py
@@ -0,0 +1,6 @@
+# input num1 and num2 
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# get the difference of the numbers and print
+print(f"The difference of the two numbers is: {num1 - num2}")
