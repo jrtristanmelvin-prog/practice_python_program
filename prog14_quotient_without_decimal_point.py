8330cc84711cf61f13d844119a209647eca057df
Tristanjr <jrtristanmelvin@gmail.com>
2026-03-20 23:31:06 +0800

    create program that print the remainder'

--- prog14_quotient_without_decimal_point.py
+++ prog14_quotient_without_decimal_point.py
@@ -1,6 +1,6 @@
 # input num1 and num2 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 #  get the quotient and print the result without decimal point
 print(f"The result is: {num1 // num2 }")
---
+++ prog15_remainder.py
@@ -0,0 +1,6 @@
+# input num1 and num2
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# divide num1 by numm2 and print the remainder
+print(f"The remainder is: {num1 % num2}")
